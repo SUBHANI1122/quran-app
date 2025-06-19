5d337da74ad5bb401aa659893e1cd973aa37f9b9
Talha Javed <talha.javed@365caresupport.com>
2025-06-19 19:06:39 +0500

    this is daily command running

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('send:daily-topic')->everyMinute();
+Schedule::command('send:daily-topic')->hourly();
