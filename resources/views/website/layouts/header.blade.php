c29ceea033192abecda78a1328429f0741c6bd94
Talha Javed <talha.javed@365caresupport.com>
2025-06-20 18:41:12 +0500

    quran pak home page is updted

--- resources/views/website/layouts/header.blade.php
+++ resources/views/website/layouts/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="col col-lg-3">
                     <div class="logo">
                         <a href="index.php" class="">
-                            <img src="{{ asset('assets/img/logo.png') }}" alt="Dome Carlow">
+                            QURAN PAK
                         </a>
                     </div>
                 </div>
@@ -15,22 +15,3 @@
         </div>
     </div>
 </header>
-<section class="header-steps">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-9 offset-lg-3">
-                <div class="stepper">
-                    <div class="stepper-item active">
-                        <span class="stepper-label">1. Choose Activity</span>
-                    </div>
-                    <div class="stepper-item">
-                        <span class="stepper-label">2. Persoanl Information</span>
-                    </div>
-                    <div class="stepper-item">
-                        <span class="stepper-label">3. Payment Details</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
